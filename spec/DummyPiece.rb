5c226281081502342265869f4c04ad18e4349683
Jun Huang <1624698+JunYuHuang@users.noreply.github.com>
2023-06-22 13:34:23 -0700

    Add explanation comment for DummyPiece class

--- spec/DummyPiece.rb
+++ spec/DummyPiece.rb
@@ -1,5 +1,10 @@
 require './lib/Piece'
 
+=begin
+This `Piece` subclass is a wildcard Piece that can be any valid chess piece (e.g. PawnPiece) or a piece that represents an empty cell on the board (i.e. EmptyPiece).
+`DummyPiece` is meant to be used as a mock or test double in the testing or spec files only and not in or as part of a real chess game.
+=end
+
 class DummyPiece < Piece
   attr_accessor(:default_options, :did_move)
 
